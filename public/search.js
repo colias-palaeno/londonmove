515afced35f197e8a6a29581a449e16bbe7641d5
ALIAS <69894931+colias-palaeno@users.noreply.github.com>
2022-09-25 08:39:28 +0100

    Add files via upload
    Refactored code

--- public/search.js
+++ public/search.js
@@ -2,8 +2,10 @@ let boroughList = [], toRender = [];
 
 (async () => {
     await fetch('boroughs.json').then(resp => resp.json()).then(json => boroughList = json.map(b=>b[0]));
-    boroughList.forEach(borough => {
-        toRender.push(m('option', {value: borough}, borough.replace(/(\b[a-z](?!\s))/g, e=>e.toUpperCase()).replaceAll('-', ' ')))
-    });
+    boroughList.forEach(borough => 
+        toRender.push(
+            m('option', {value: borough}, borough.replace(/(\b[a-z](?!\s))/g, e=>e.toUpperCase()).replaceAll('-', ' '))
+        )
+    );
     m.render(document.getElementById('loc'), toRender)
 })()
--- public/submitted.js
+++ public/submitted.js
@@ -1,18 +1,18 @@
 const borough = this.location.pathname.slice(8),
-boroughCookie = Cookies.get(borough)
+boroughCookie = Cookies.get(borough),
 results = document.getElementById('results'),
 now = new Date(),
 getDate = offset => {
     if (now.getMonth()-offset < 0) offset -= 12
     return `${now.getFullYear()}-${(now.getMonth()+1-offset+'').padStart(2, "0")}-01`
 },
-landRegURL = 
-`https://landregistry.data.gov.uk/app/ukhpi/download/new.json?from=${getDate(3)}&to=${getDate(2)}&location=http://landregistry.data.gov.uk/id/region/`,
+landRegURL = `https://landregistry.data.gov.uk/app/ukhpi/download/new.json?from=${getDate(3)}&to=${getDate(2)}&location=http://landregistry.data.gov.uk/id/region/`,
 getCrimeDataURL = (lat, lng) => `https://data.police.uk/api/crimes-street/all-crime?date=${now.getFullYear()-1}-${now.getMonth()+1}&lat=${lat}&lng=${lng}`
 
 document.querySelector('heading').innerHTML += ` - ${borough.replaceAll('-', ' ').replace(/\b\w/g, c => c.toUpperCase())}` 
 
-if (boroughCookie != null) { // checks if there's a cookie with info on this EXACT borough
+if (boroughCookie != null) 
+{ // checks if there's a cookie with info on this EXACT borough
     const parsedBoroughCookie = JSON.parse(boroughCookie)
     m.render(results, m('span', [
         'Average Property Prices: ',
@@ -21,10 +21,12 @@ if (boroughCookie != null) { // checks if there's a cookie with info on this EXA
         'Crime Count: ',
         m('b', parsedBoroughCookie[2])
     ]))
-} else {
+} 
+
+else 
+{
     const pricesCache = Cookies.get('pricesCache')
-    let dataToRender = [],
-    boroughCoords;
+    let dataToRender = [], boroughCoords;
 
     (async () => {
         await fetch('../boroughs.json')
@@ -37,11 +39,12 @@ if (boroughCookie != null) { // checks if there's a cookie with info on this EXA
         .then(resp => resp.json())
         .then(json => dataToRender.push(json.length.toLocaleString()))
 
-        if (pricesCache != null) { // checks if there's a cookie with prices from the homepage (info on all boroughs)
+        if (pricesCache != null) 
+        { // checks if there's a cookie with prices from the homepage (info on all boroughs)
             const indexOfBorough = JSON.parse(pricesCache).findIndex(el => el.includes(borough)),
             boroughInPricesCache = JSON.parse(pricesCache)[indexOfBorough]
             dataToRender.unshift(`(No. ${indexOfBorough+1})`)
-            m.render(results, [
+            m.render(results, 
                 m('span', [
                     'Average Property Prices: ',
                     m('b', '£'+boroughInPricesCache[1].toLocaleString()),
@@ -49,30 +52,32 @@ if (boroughCookie != null) { // checks if there's a cookie with info on this EXA
                     dataToRender[0], m('br'),
                     'Crime Count: ',
                     m('b', dataToRender[1])
-                ]),
-                
-            ])
-        } else {
-            fetch(landRegURL + borough)
-            .catch(e => m.render(results, [m(`div', 'Error: ${e}.`), m('br'), m('Please try refreshing the page.')]))
-            .then(resp => resp.json())
-            .then(json => {
-                let prices = [json.items[0]['ukhpi:averagePrice'][0], json.items[1]['ukhpi:averagePrice'][0]],
-                higherPrice = 1
-                if (prices[0] > prices[1]) higherPrice = 0
-                dataToRender.unshift('£'+prices[0].toLocaleString(), higherPrice)
-
-                Cookies.set(borough, JSON.stringify(dataToRender), {expires: 31})
-                m.render(results, [
-                    m('span', [
-                        'Average Property Prices: ',
-                        m('b', dataToRender[0]),
-                        m('img', {src: `../arrow${dataToRender[1]}.png`}), m('br'),
-                        'Crime Count: ',
-                        m('b', dataToRender[2])
-                    ])
                 ])
-            })
+            )
+        } 
+        
+        else 
+        {
+            fetch(landRegURL + borough)
+                .catch(e => m.render(results, [m(`div', 'Error: ${e}.`), m('br'), m('Please try refreshing the page.')]))
+                .then(resp => resp.json())
+                    .then(json => {
+                        const prices = [
+                            json.items[0]['ukhpi:averagePrice'][0], json.items[1]['ukhpi:averagePrice'][0]
+                        ], higherPrice = (prices[0] < prices[1]) ? 1 : 0
+                        dataToRender.unshift('£'+prices[0].toLocaleString(), higherPrice)
+
+                        Cookies.set(borough, JSON.stringify(dataToRender), {expires: 31})
+                        m.render(results, [
+                            m('span', [
+                                'Average Property Prices: ',
+                                m('b', dataToRender[0]),
+                                m('img', {src: `../arrow${dataToRender[1]}.png`}), m('br'),
+                                'Crime Count: ',
+                                m('b', dataToRender[2])
+                            ])
+                        ])
+                    })
         }
     })()
 }
